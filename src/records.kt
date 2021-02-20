318a07e16244a60e183430eab4bc9ee66a957f96
juliet879 <gisembajuliett@gmail.com>
2021-02-20 23:59:31 +0300

    add boolean

--- src/records.kt
+++ src/records.kt
@@ -3,10 +3,12 @@ fun main() {
     var age = "21" + "years"
     var phoneNumber = "0705699879"
     var weight = "56" + "Kgs"
+    val Kenyan = true
 
 
     println(names)
     println(age)
     println(phoneNumber)
     println(weight)
+    println(Kenyan)
 }
