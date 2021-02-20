bddee49cbded1d72a1250d2c9f83a886f64cb00b
juliet879 <gisembajuliett@gmail.com>
2021-02-20 23:15:24 +0300

    add variables

---
+++ src/records.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val names = "Juliet Kemunto Gisemba"
+    var age = "21" + "years"
+    var phoneNumber = "0705699879"
+    var weight = "56" + "Kgs"
+
+
+    println(names)
+    println(age)
+    println(phoneNumber)
+    println(weight)
+}
