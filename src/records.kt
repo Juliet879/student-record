6d18e4f0cc99d81dde1002fb433107c8d06e0152
juliet879 <gisembajuliett@gmail.com>
2021-02-21 17:23:18 +0300

    add boolean

--- src/records.kt
+++ src/records.kt
@@ -2,13 +2,16 @@ fun main() {
     val names = "Juliet Kemunto Gisemba"
     var age = "21" + "years"
     var phoneNumber = "0705699879"
-    var weight = "56" + "Kgs"
-    val Kenyan = true
+    var weight = 56.toFloat()
+    var kenyan = true
 
 
     println(names)
     println(age)
     println(phoneNumber)
     println(weight)
-    println(Kenyan)
+    println(kenyan)
 }
+
+
+
